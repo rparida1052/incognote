ceca825a7325fbcd7833cbcbc30e7c46c724885b
Rahul Parida <rparida1052@gmail.com>
2024-07-11 17:10:41 +0530

    ui fixes

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -62,9 +62,9 @@ export default function SignInForm() {
       <div className="w-full max-w-md p-8 space-y-8 bg-white rounded-lg shadow-md">
         <div className="text-center">
           <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl mb-6">
-            Welcome Back to True Feedback
+            Welcome Back to IncogNote
           </h1>
-          <p className="mb-4">Sign in to continue your secret conversations</p>
+          <p className="mb-4">Sign in to collect secret note</p>
         </div>
         <Form {...form}>
           <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -83,7 +83,7 @@ const SignUp = () => {
     <div className="w-full max-w-md p-8 space-y-8 bg-white rounded-lg shadow-md">
       <div className="text-center">
         <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl mb-6">
-          Join True Feedback
+          Join IncogNote
         </h1>
         <p className="mb-4">Sign up to start your anonymous adventure</p>
       </div>
@@ -125,7 +125,7 @@ const SignUp = () => {
               <FormItem>
                 <FormLabel>Email</FormLabel>
                 <Input {...field} name="email" />
-                <p className='text-muted text-gray-400 text-sm'>We will send you a verification code</p>
+                <p className='text-muted text-gray-800 text-sm'>*We will send you a verification code</p>
                 <FormMessage />
               </FormItem>
             )}
